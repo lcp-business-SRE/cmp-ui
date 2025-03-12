541f278ab678d4ea952a474f860bfbc0a1d6a9a0
Seungha Ock <seungha.ock@mb.lecip.co.jp>
2025-03-12 00:18:36 +0000

    quasarモジュール追加

---
+++ quasar/src/boot/register-component.js
@@ -0,0 +1,6 @@
+// import MyComponent from '../component/MyComponent.vue';
+
+// // we globally register our component with Vue
+// export default ({ app }) => {
+//   app.component('my-component', MyComponent);
+// };
